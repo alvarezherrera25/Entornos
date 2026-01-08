ec33383f2e38324cddb18142b74aa1e20b6bc16e
Hugo <alvarezherrera25@iespabloserrano.org>
2026-01-08 08:50:33 +0100

    borrar scanner

--- clasificador_edades/src/clasificador_edades/clasificador.java
+++ clasificador_edades/src/clasificador_edades/clasificador.java
@@ -3,21 +3,18 @@ import java.util.Scanner;
 public class ClasificadorTryCatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int edad = -1; // Inicializamos con -1 para que entre al bucle
+        int edad = -1; 
 
         System.out.println("--- Clasificador de Edad (Con Try-Catch) ---");
 
         do {
             System.out.print("Introduce una edad (0 para salir): ");
             
-            // 1. Leemos la entrada como TEXTO (String)
             String textoUsuario = scanner.next();
 
             try {
-                // 2. Intentamos convertir ese texto a número
                 edad = Integer.parseInt(textoUsuario);
 
-                // --- Si la conversión funciona, se ejecuta tu lógica: ---
                 if (edad == 0) {
                     System.out.println("Saliendo...");
                 } else if (edad < 0) {
@@ -33,10 +30,8 @@ public class ClasificadorTryCatch {
                 }
 
             } catch (NumberFormatException e) {
-                // 3. Si pusieron letras, JAVA lanza un error y cae aquí inmediatamente
                 System.out.println(">> Error: '" + textoUsuario + "' contiene letras o símbolos.");
                 
-                // Aseguramos que edad sea diferente de 0 para que el bucle no termine por error
                 edad = -1; 
             }
             
@@ -44,6 +39,5 @@ public class ClasificadorTryCatch {
 
         } while (edad != 0);
 
-        scanner.close();
     }
 }
